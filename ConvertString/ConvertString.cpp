aafa6be930de4e8df80fc2c81535e5d95a47f25a
user <user@DESKTOP-977GA6F>
2021-12-10 01:58:54 +0300

    Добавьте файлы проекта.

---
+++ ConvertString/ConvertString.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <map>
+using namespace std;
+
+int wmain()
+{
+    locale::global(locale(""));
+    wcin.imbue(locale("." + std::to_string(GetConsoleCP())));
+    
+    wcout << "Please enter a string to convert:\n";
+    wstring string_to_convert;
+    getline(wcin, string_to_convert);
+
+    wstring converted_string = string_to_convert;
+
+    auto& facet = use_facet<ctype<wchar_t>>(locale());
+    facet.tolower(&converted_string[0], &converted_string[0] + converted_string.size());
+
+    map<wchar_t, int> numbers;
+    for (wchar_t& c : converted_string) {
+        numbers[c]++;
+    }
+
+    for (wchar_t& c : converted_string) {
+        if (numbers[c] > 1)
+            c = ')';
+        else
+            c = '(';
+    }
+
+    wcout << "\nResult:\n";
+    wcout << converted_string << endl;
+
+    cin.get();
+    return 0;
+}
